8b7bdbaf878d7ba56ca2857bcddcbdbfa8dd0673
Sainayan <sainayan@jugyah.com>
2023-09-26 11:37:54 +0530

    async

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,21 @@
 // @ts-nocheck
 'use client';
+import { useEffect } from 'react';
 
 import { getAPI } from './FetchUtil';
 
 export default function Home() {
-  const data = getAPI();
-  console.log(data);
+  const fetchAPI = async () => {
+    const data = await getAPI();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
   return (
     <>
       <div>well hello</div>
-      {data && <div>{data.data}</div>}
     </>
   );
 }
